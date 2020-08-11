005faec5ba0539952a0f324aa135bdc36c04d433
CameronWill <cellardoorsolar@aol.com>
2020-08-10 18:37:04 -0700

    complete readme
    Co-Authored-By: Sean Wilson <paintballsean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,5 @@ function add(number1, number2) {
   const number1 = parseInt(prompt("Enter a number:"));
   const number2 = parseInt(prompt("Enter another number:"));
   const result = add(number1, number2);
+
   alert (result);
