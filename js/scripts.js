bc75ed40cc7162d51c44e81fe767bd2982317d4c
CameronWill <cellardoorsolar@aol.com>
2020-08-12 13:52:01 -0700

    add all functions to html with singular output box
    Co-Authored-By: Sean Wilson <paintballsean@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,23 +20,23 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
+    $(".output").text(result);
   });
 
-  $("form#subtract").submit(function(event) {
+  $("form#sub").submit(function(event) {
     event.preventDefault();
-    const number1 = parseInt($("#subtract1").val());
-    const number2 = parseInt($("#subtract2").val());
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
     const result = subtract(number1, number2);
-    $("#output").text(result);
+    $(".output").text(result);
   });
 
-  $("form#muliply").submit(function(event) {
+  $("form#multiply").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#multiply1").val());
     const number2 = parseInt($("#multiply2").val());
     const result = multiply(number1, number2);
-    $("#output").text(result);
+    $(".output").text(result);
   });
 
   $("form#divide").submit(function(event) {
@@ -44,6 +44,7 @@ $(document).ready(function() {
     const number1 = parseInt($("#divide1").val());
     const number2 = parseInt($("#divide2").val());
     const result = divide(number1, number2);
-    $("#output").text(result);
+    $(".output").text(result);
   });
+
 });
